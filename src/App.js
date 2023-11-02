d15e9c234f30156b0327d9ed91bbe6555d4e3934
Tarun <tarun.coderfarm@gmail.com>
2023-11-02 17:45:41 +0530

    Secondd responsive page complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-import Firstpage from './Components/FirstPage/Firstpage';
+// import Firstpage from './Components/FirstPage/Firstpage';
 import  '../src/index';
 // import Thirdpage from './Components/ThirdPage/Thirdpage';
-// import Secondpage from './Components/SecondPage/Secondpage';
+import Secondpage from './Components/SecondPage/Secondpage';
 
 function App() {
   return (
     <div className="App">
-     <Firstpage/>
-     {/* <Secondpage/> */}
+     {/* <Firstpage/> */}
+     <Secondpage/>
   {/* <Thirdpage/> */}
     </div>
   );
