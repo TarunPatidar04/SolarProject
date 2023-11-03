836b965d08c44a67903551695c25829134f0600b
Tarun <tarun.coderfarm@gmail.com>
2023-11-03 12:09:10 +0530

    rresponsive page completee

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0dpU5xk_tKy1JeGyzRb0qmocayYHzSNU",
+  authDomain: "solarproject-2169a.firebaseapp.com",
+  projectId: "solarproject-2169a",
+  storageBucket: "solarproject-2169a.appspot.com",
+  messagingSenderId: "600066581093",
+  appId: "1:600066581093:web:2c369c193ddad29f049c8c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
